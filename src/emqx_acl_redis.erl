efeb04462602e4f6d3e1ecf888e673b9d050e09b
周学浩(10033362) <zhouxh-e@glodon.com>
2021-03-05 19:24:05 +0800

    fix bug for user acl

--- src/emqx_acl_redis.erl
+++ src/emqx_acl_redis.erl
@@ -28,30 +28,32 @@ check_acl(ClientInfo, PubSub, Topic, AclResult, Config) ->
         {stop, deny} -> emqx_metrics:inc(?ACL_METRICS(deny)), {stop, deny}
     end.
 
-%% Binary bits match: default type is integer, default size is 8, default unit is 1
 do_check_acl(#{username := <<$$, _/binary>>}, _PubSub, _Topic, _AclResult, _Config) -> ok;
 do_check_acl(#{username := <<"dashboard">>}, _PubSub, _Topic, _AclResult, _Config) -> ok;
-do_check_acl(#{username := Uid, clientid := <<$^, _/bytes>>}, _PubSub, <<"enno", _/binary>> = Topic, _AclResult, _Config) ->
-    ?LOG_GLD("ACL Tenant ZL-IoT-2.0 UID: ~s, Topic: ~s", [Uid, Topic]), {stop, allow};
-do_check_acl(#{username := Uid, clientid := <<$^, _/bytes>>, password := Token}, PubSub, Topic, _AclResult, _Config) ->
-    ?LOG_GLD("ACL Tenant ZL-IoT-2.0 UID: ~s, Token: ~s, Topic: ~s", [Uid, Token, Topic]),
-    [_Prefix| Rest] = emqx_topic:words(Topic),
-    [PrdId| Rest1] = Rest,
-    [DevId| _Rest2] = Rest1,
+do_check_acl(#{clientid := <<$^, _/bytes>>}, _PubSub, <<"enno", _/binary>> = Topic, _AclResult, _Config) -> {stop, allow};
+do_check_acl(#{username := UT, clientid := <<$^, _/bytes>>},  PubSub, Topic, _AclResult, _Config) ->
+    ?LOG_GLD("ACL Tenant ZL-IoT-2.0 username&token: ~s, Topic: ~s~n", [UT, Topic]),
+    [_Prefix, PrdId , DevId | _Rest] = emqx_topic:words(Topic),
     {ok, Path} = application:get_env(?WEB_HOOK_APP, acl_path),
     {ok, Headers} = application:get_env(?WEB_HOOK_APP, headers, []),
-    Params = #{deviceId => DevId},
-    NHeaders = [{<<"Authorization">>, <<"bearer ", Token/binary>>} | Headers],
-    case emqx_auth_hook:send_http_request(Uid, Params, Path, Headers, post) of
-	{ok, RawData} -> 
-	    ?LOG_GLD("ACL WebHook Rsp OK: ~p", [RawData]),
-	    acl_match(PubSub, Topic, DevId, PrdId, 1);
-	{error, ErrMsg} -> 
-	    ?LOG_GLD("ACL WebHook Rsp Err: ~p", [ErrMsg]),
+    case binary:split(UT,<<$&>>,[global]) of
+	[Uid,Token] ->
+	    Params = #{deviceId => DevId},
+	    NHeaders = [{<<"Authorization">>, <<"bearer ", Token/binary>>} | Headers],
+	    case emqx_auth_hook:send_http_request(Uid, Params, Path, Headers, post) of
+		{ok, RawData} -> 
+		    ?LOG_GLD("ACL WebHook Rsp OK: ~p", [RawData]),
+		    acl_match(PubSub, Topic, DevId, PrdId, 0);
+		{error, ErrMsg} -> 
+		    ?LOG_GLD("ACL WebHook Rsp Err: ~p", [ErrMsg]),
+		    {stop, deny}
+	    end;
+	_ ->
+	    ?LOG_GLD("ACL ZL-IoT-2.0: Invalid username: ~s", [UT]),
 	    {stop, deny}
     end;
-%% do_check_acl(#{username := DevPrdTs, clientid := <<0:2,_Rest/bits>>}, PubSub, Topic, _AclResult, _Config) ->
 do_check_acl(#{username := DevPrdTs}, PubSub, Topic, _AclResult, _Config) ->
+    ?LOG_GLD("ACL Device ZL-IoT-2.0 DeviceId: ~s, Topic: ~s~n", [DevPrdTs, Topic]),
     case binary:split(DevPrdTs,<<$&>>,[global]) of
 	%% ZL-2.0
 	[DevId,PrdId,_Ts] ->
@@ -61,7 +63,7 @@ do_check_acl(#{username := DevPrdTs}, PubSub, Topic, _AclResult, _Config) ->
 	[DevPrdTs] -> 
 	    ?LOG_GLD("ACL Redis ZL-IoT-1.0: DevPrdTs: ~p", [DevPrdTs]),
 	    acl_match(PubSub, Topic, DevPrdTs, null, 0);
-	 _ ->
+	_ ->
 	    ?LOG_GLD("ACL Redis ZL-IoT-1.0: Invalid DevPrdTs: ~s", [DevPrdTs]),
 	    {stop, deny}
     end.
@@ -89,8 +91,8 @@ match(_, _, []) -> nomatch;
 match(PubSub, Topic, [{Filter, Access}| Rules]) ->
     %% case {match_topic(Topic, Filter), match_access(PubSub, b2i(Access))} of
     case {match_topic(Topic, Filter), match_access(PubSub, Access)} of
-        {true, true} -> allow;
-        {_, _} -> match(PubSub, Topic, Rules)
+	{true, true} -> allow;
+	{_, _} -> match(PubSub, Topic, Rules)
     end.
 
 match_topic(Topic, Filter) -> emqx_topic:match(Topic, Filter).
